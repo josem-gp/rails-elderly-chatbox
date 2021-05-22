0f11cee4682942576d7de542d485b4152b81f920
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-05-22 18:49:05 +0900

    Added basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show] do
+    resources :messages, only: [:new, :create]
+  end
+  resources :rooms, only: [:show] do
+    resources :messages, only: [:index]
+  end
+  # I want the user to create a new user --> once the user is created, it will be assigned to the corresponding rooms (4)
+  # I want the user to go to his profile page if the user has been created
+  # I want the user to be able to create a message & edit & destroy it (only if it is its owner)
+
+  # I want the user to be able to enter a new room
+  # I want the user to be able to upvote messages
+  # I want the room to display the messages written
 end
