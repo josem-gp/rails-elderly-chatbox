4570100d79825bb2d51323a6ff7d7568794dc3bc
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-05-24 21:48:29 +0900

    Improve choosing of rooms and sending messages

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,7 +2,7 @@ class ParticipantsController < ApplicationController
   def new
     @user = User.find(params[:user_id])
     @participant = Participant.new
-    @rooms = Room.where(name: @user.municipality.match(/(\w*)-/)[1].strip).or(Room.where(room_type: 'public'))
+    @rooms = Room.where.not(id: @user.room_ids)
   end
 
   def create
