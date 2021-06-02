a4336d0971a5e914e31ca16151bcd38d50fc7ba6
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-02 22:07:07 +0900

    Improved JS pic selection

--- app/javascript/components/init_selectShop.js
+++ app/javascript/components/init_selectShop.js
@@ -6,8 +6,9 @@ const selectShop = () => {
     shop.addEventListener('click', (event) => {
       event.preventDefault();
       const fullId = event.currentTarget.id;
-      const id = fullId.match(/(marker-shop-)(\d)/)[2];
+      const id = fullId.match(/(marker-shop-)(\d+)/)[2];
       form.value = id;
+
 
       const markers = JSON.parse(document.getElementById('map').dataset.markers);
       const marker = markers.find(marker => marker.id == id);
