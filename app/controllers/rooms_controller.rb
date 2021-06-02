857f26283b64b9a636ef0bbc566abef175927ae7
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-03 00:40:50 +0900

    Finished Thread-like chatlogic

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,7 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     @user = User.find(params[:user_id])
+    @shops = Shop.all
     authorize @room
   end
 end
