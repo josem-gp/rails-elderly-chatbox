c3acb94dfd0b346a36f121ed25e270b25485ef95
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-13 17:18:03 +0900

    implemented search function

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,12 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     @user = User.find(params[:user_id])
-    @shops = Shop.all
+    @shops =
+    if params[:query]
+      Shop.search_by_name_and_address(params[:query])
+    else
+      Shop.all
+    end
     authorize @room
   end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,11 @@ class Shop < ApplicationRecord
   has_many :messages
   after_validation :geocode, if: :will_save_change_to_address?
   has_many_attached :photos
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_address,
+    against: [ :name, :address ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
