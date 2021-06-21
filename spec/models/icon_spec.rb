5f0db712d1a3405ae0c12d14e3a52ad7ccaa7282
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-21 21:31:00 +0900

    added rspec model icon

---
+++ spec/models/icon_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Icon, type: :model do
+  it "is valid with a url from the list" do
+    icon = Icon.new(name: "icon1",
+                    url: Icon::IMAGES[0])
+    expect(icon).to be_valid
+  end
+  it "is not valid with a url that is not in the list" do
+    icon = Icon.new(name: "Chiyoda",
+                    url: "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg")
+    icon.valid?
+    expect(icon.errors[:url]).to include("is not included in the list")
+  end
+end
