93484b6281d73333e5f3686c645d664cc04616d9
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-25 20:27:14 +0900

    added reader from api to db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,30 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
   def home
+    admin = User.find_by(name: 'admin')
+    if admin
+      participants = admin.participants
+      participants.destroy_all
+      admin.delete
+      Shop.destroy_all
+    end
+    admin = User.create!(name: 'admin', age: 28, municipality: 'Chuo-ku', alias: 'admin', email: 'admin@hotmail.com', password: '1234567')
+    require "open-uri"
+    require "json"
+    response = open("https://good-neighbour-api.herokuapp.com/api/v1/shops").read
+    json = JSON.parse(response)
+    json.each do |shop|
+      name = shop['name']
+      address = shop['address']
+      phone_number = shop['phone_number']
+      website = shop['website']
+      image = shop['images']
+
+      file = URI.open(image)
+      shop_seed = Shop.new(name: name, address: address, phone_number: phone_number, website: website, user_id: admin)
+      shop_seed.photos.attach(io: file, filename: 'shop.jgp', content_type: 'image/jgp')
+      shop_seed.save!
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require "open-uri"
+# require "open-uri"
 
 def cleaning_name(address)
   address.match(/(\w*)-/)[1].strip
