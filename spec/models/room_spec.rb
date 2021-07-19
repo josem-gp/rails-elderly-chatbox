f0a33ec19c034dff5aace26cb692e4c1fc89a11a
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-19 17:25:10 +0900

    use of described_class for room_spec

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -4,27 +4,27 @@ require 'rails_helper'
 
 RSpec.describe Room, type: :model do
   before do
-    @room = Room.create(name: "Chiyoda",
-                        room_type: "private")
+    @room = described_class.create(name: "Chiyoda",
+                                   room_type: "private")
   end
   it "is valid with a name and room_type" do
-    room = Room.new(name: "Adachi",
-                    room_type: "private")
+    room = described_class.new(name: "Adachi",
+                               room_type: "private")
     expect(room).to be_valid
   end
   it "doesnt allow duplicate names" do
-    room = Room.new(name: "Chiyoda",
-                    room_type: "private")
+    room = described_class.new(name: "Chiyoda",
+                               room_type: "private")
     room.valid?
     expect(room.errors[:name]).to include("has already been taken")
   end
   it "is invalid without a name" do
-    room = Room.new(name: nil)
+    room = described_class.new(name: nil)
     room.valid?
     expect(room.errors[:name]).to include("can't be blank")
   end
   it "is invalid without a room_type" do
-    room = Room.new(room_type: nil)
+    room = described_class.new(room_type: nil)
     room.valid?
     expect(room.errors[:room_type]).to include("can't be blank")
   end
