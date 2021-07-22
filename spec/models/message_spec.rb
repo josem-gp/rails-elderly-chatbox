28875d6413d079d43b6350032354cc1edfed0e3b
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-22 16:16:55 +0900

    created first describe inside the message spec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -9,54 +9,44 @@ RSpec.describe Message, type: :model do
   let(:user) { User.create(name: "Tester", alias: "Test_alias",
                            municipality: "Chiyoda-ku", email: "tester@example.com",
                            password: "1234567", icon: icon) }
+  let(:shop) { Shop.create(name: 'Test', address: 'Test address',
+                           phone_number: 'Test number', website: 'Test url')}
 
-  # before do
-  #   icon = Icon.create(name: 'default_icon', url: Icon::IMAGES[0])
-  #   @room = Room.new(name: "Chiyoda",
-  #                    room_type: "private")
-  #   @public_room = Room.new(name: "General",
-  #                           room_type: "public")
-  #   @user = User.new(name: "Tester",
-  #                    alias: "Test_alias",
-  #                    municipality: "Chiyoda-ku",
-  #                    email: "tester@example.com",
-  #                    password: "1234567",
-  #                    icon: icon)
-  #   @shop = Shop.new(name: 'Test', address: 'Test address', phone_number: 'Test number', website: 'Test url')
-  # end
-  it "is valid with a title, content, votes, user_id, shop_id and room_id" do
-    message = described_class.new(title: 'Message', content: 'This place is so good!', vote: 1, user: @user, room: @room, shop: @shop)
-    expect(message).to be_valid
-  end
-  it "must have a title" do
-    message = described_class.new(content: 'This place is so good!', vote: 1, user: @user, room: @room, shop: @shop)
-    message.valid?
-    expect(message.errors[:title]).to include("can't be blank")
-  end
-  it "must have a content" do
-    message = described_class.new(title: 'Test', vote: 1, user: @user, room: @room, shop: @shop)
-    message.valid?
-    expect(message.errors[:content]).to include("can't be blank")
-  end
-  it "must have a content with more than 5 characters" do
-    message = described_class.new(title: 'Message', content: 'Test', vote: 1, user: @user, room: @room, shop: @shop)
-    message.valid?
-    expect(message.errors[:content]).to include('is too short (minimum is 5 characters)')
-  end
-  it "must have a room_id" do
-    message = described_class.new(title: 'Test', content: 'This place is so good!', vote: 1, user: @user, shop: @shop)
-    message.valid?
-    expect(message.errors[:room]).to include("must exist")
-  end
-  it "must have a user_id" do
-    message = described_class.new(title: 'Test', content: 'This place is so good!', vote: 1, room: @room, shop: @shop)
-    message.valid?
-    expect(message.errors[:user]).to include("must exist")
-  end
-  it "must have a shop_id" do
-    message = described_class.new(title: 'Test', content: 'This place is so good!', vote: 1, room: @room, user: @user)
-    message.valid?
-    expect(message.errors[:shop]).to include("must exist")
+  describe 'event model initialization' do
+    it "is valid with a title, content, votes, user_id, shop_id and room_id" do
+      message = described_class.new(title: 'Message', content: 'This place is so good!', vote: 1, user: @user, room: @room, shop: @shop)
+      expect(message).to be_valid
+    end
+    it "must have a title" do
+      message = described_class.new(content: 'This place is so good!', vote: 1, user: @user, room: @room, shop: @shop)
+      message.valid?
+      expect(message.errors[:title]).to include("can't be blank")
+    end
+    it "must have a content" do
+      message = described_class.new(title: 'Test', vote: 1, user: @user, room: @room, shop: @shop)
+      message.valid?
+      expect(message.errors[:content]).to include("can't be blank")
+    end
+    it "must have a content with more than 5 characters" do
+      message = described_class.new(title: 'Message', content: 'Test', vote: 1, user: @user, room: @room, shop: @shop)
+      message.valid?
+      expect(message.errors[:content]).to include('is too short (minimum is 5 characters)')
+    end
+    it "must have a room_id" do
+      message = described_class.new(title: 'Test', content: 'This place is so good!', vote: 1, user: @user, shop: @shop)
+      message.valid?
+      expect(message.errors[:room]).to include("must exist")
+    end
+    it "must have a user_id" do
+      message = described_class.new(title: 'Test', content: 'This place is so good!', vote: 1, room: @room, shop: @shop)
+      message.valid?
+      expect(message.errors[:user]).to include("must exist")
+    end
+    it "must have a shop_id" do
+      message = described_class.new(title: 'Test', content: 'This place is so good!', vote: 1, room: @room, user: @user)
+      message.valid?
+      expect(message.errors[:shop]).to include("must exist")
+    end
   end
   it "is valid for a user to have several messages" do
     message = described_class.create!(title: 'Message', content: 'This place is so good!', vote: 1, user: @user, room: @room, shop: @shop)
