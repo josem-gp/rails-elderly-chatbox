62dad4d0e4df72944e48f103778ffae9f78ce617
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-22 18:20:02 +0900

    create different describe & refactor in room spec

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -4,48 +4,53 @@ require 'rails_helper'
 
 RSpec.describe Room, type: :model do
 
-  let!(:room) { described_class.create(name: "Chiyoda", room_type: "private") }
+  describe 'room model initialization' do
+    it "is valid with a name and room_type" do
+      room = described_class.new(name: "Adachi",
+                                 room_type: "private")
+      expect(room).to be_valid
+    end
+    it "doesnt allow duplicate names" do
+      first_room = described_class.create(name: "Chiyoda", room_type: "private")
+      second_room = described_class.new(name: "Chiyoda",
+                                        room_type: "private")
+      second_room.valid?
+      expect(second_room.errors[:name]).to include("has already been taken")
+    end
+    it "is invalid without a name" do
+      room = described_class.new(name: nil)
+      room.valid?
+      expect(room.errors[:name]).to include("can't be blank")
+    end
+    it "is invalid without a room_type" do
+      room = described_class.new(room_type: nil)
+      room.valid?
+      expect(room.errors[:room_type]).to include("can't be blank")
+    end
+  end
 
-  it "is valid with a name and room_type" do
-    room = described_class.new(name: "Adachi",
-                               room_type: "private")
-    expect(room).to be_valid
-  end
-  it "doesnt allow duplicate names" do
-    first_room = described_class.create(name: "Chiyoda", room_type: "private")
-    second_room = described_class.new(name: "Chiyoda",
-                                      room_type: "private")
-    second_room.valid?
-    expect(second_room.errors[:name]).to include("has already been taken")
-  end
-  it "is invalid without a name" do
-    room = described_class.new(name: nil)
-    room.valid?
-    expect(room.errors[:name]).to include("can't be blank")
-  end
-  it "is invalid without a room_type" do
-    room = described_class.new(room_type: nil)
-    room.valid?
-    expect(room.errors[:room_type]).to include("can't be blank")
-  end
-  it "allows a room to have several users" do
-    icon = Icon.create!(name: 'default_icon', url: Icon::IMAGES[0])
-    user = User.create!(name: "Tester",
-                        alias: "Test_alias",
-                        municipality: "Chiyoda-ku",
-                        email: "tester@example.com",
-                        password: "1234567",
-                        icon: icon)
-    icon = Icon.create!(name: 'default_icon', url: Icon::IMAGES[0])
-    user1 = User.create!(name: "Tester1",
-                         alias: "Test_alias1",
-                         municipality: "Chiyoda-ku",
-                         email: "tester1@example.com",
-                         password: "1234567",
-                         icon: icon)
-    participant = Participant.create(room: room, user: user)
-    participant1 = Participant.create(room: room, user: user1)
+  describe "event model instantiation" do
+    let!(:room) { described_class.create(name: "Chiyoda", room_type: "private") }
 
-    expect(participant1.room).to be_valid
+    it "allows a room to have several users" do
+      icon = Icon.create!(name: 'default_icon', url: Icon::IMAGES[0])
+      user = User.create!(name: "Tester",
+                          alias: "Test_alias",
+                          municipality: "Chiyoda-ku",
+                          email: "tester@example.com",
+                          password: "1234567",
+                          icon: icon)
+      icon = Icon.create!(name: 'default_icon', url: Icon::IMAGES[0])
+      user1 = User.create!(name: "Tester1",
+                           alias: "Test_alias1",
+                           municipality: "Chiyoda-ku",
+                           email: "tester1@example.com",
+                           password: "1234567",
+                           icon: icon)
+      participant = Participant.create(room: room, user: user)
+      participant1 = Participant.create(room: room, user: user1)
+
+      expect(participant1.room).to be_valid
+    end
   end
 end
