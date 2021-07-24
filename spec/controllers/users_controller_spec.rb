70b52732bff211c60401977f283dd3713b1fc5e1
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-24 12:03:30 +0900

    added two more tests in case the user is not signed in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,16 +3,29 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   describe "GET #show" do
     let!(:user) { FactoryBot.create(:user) }
-    it "responds succesfully" do
-      # expect(response).to have_http_status(:ok) #this is the same as the one under
-      sign_in user
-      get :show, params: { id: user.id }
-      expect(response).to have_http_status "200"
+    context "as an authenticated user" do
+      it "responds succesfully" do
+        # expect(response).to have_http_status(:ok) #this is the same as the one under
+        sign_in user
+        get :show, params: { id: user.id }
+        expect(response).to have_http_status "200"
+      end
+      it "renders 'home' succesfully" do
+        sign_in user
+        get :show, params: { id: user.id }
+        expect(response).to render_template('show')
+      end
     end
-    it "renders 'home' succesfully" do
-      sign_in user
-      get :show, params: { id: user.id }
-      expect(response).to render_template('show')
+
+    context "as a non-authenticated user" do
+      it "returns a 302 response" do
+        get :show, params: { id: user.id }
+        expect(response).to have_http_status "302"
+      end
+      it "redirects to the sign_in page" do
+        get :show, params: { id: user.id }
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 end
