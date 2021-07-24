df386befaea132e45d885856b1098a73a3be3840
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-24 17:16:22 +0900

    added tests for rooms controller

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe RoomsController, type: :controller do
+  describe "GET #show" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:room) { Room.create(name: "Chiyoda", room_type: "private") }
+    context "as an authenticated user" do
+      it "responds succesfully" do
+        sign_in user
+        get :show, params: { id: room.id, user_id: user.id}
+        expect(response).to have_http_status "200"
+      end
+      it "renders 'room' succesfully" do
+        sign_in user
+        get :show, params: { id: room.id, user_id: user.id}
+        expect(response).to render_template('show')
+      end
+    end
 
+    context "as a non-authenticated user" do
+      it "returns a 302 response" do
+        get :show, params: { id: room.id, user_id: user.id}
+        expect(response).to have_http_status "302"
+      end
+      it "redirects to the sign_in page" do
+        get :show, params: { id: room.id, user_id: user.id}
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
 end
