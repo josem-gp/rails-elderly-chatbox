8c94927a0d048ab1899aaa7384357833fc58bfed
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-24 11:22:28 +0900

    added another way to test 200 response

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe PagesController, type: :controller do
   describe "GET #home" do
     let!(:call) { get :home }
     it "responds succesfully" do
-      expect(response).to have_http_status(:ok)
+      # expect(response).to have_http_status(:ok)
+      expect(response).to have_http_status "200"
     end
     it "renders 'home' succesfully" do
       expect(response).to render_template('home')
