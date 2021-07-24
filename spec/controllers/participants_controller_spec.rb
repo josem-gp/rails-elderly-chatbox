d425f9f0e2df91bc8f6ff9af45c8eac486a94442
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-24 17:58:05 +0900

    finished specs for participants controller

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -23,26 +23,27 @@ RSpec.describe ParticipantsController, type: :controller do
       end
     end
   end
-  # describe "DELETE #destroy" do
-  #   let(:user) { FactoryBot.create(:user) }
-  #   let(:room) { Room.create(name: "Chiyoda", room_type: "private") }
-  #   context "as an authenticated user" do
-  #     it "responds succesfully" do
-  #       sign_in user
-  #       expect { delete :destroy, params: { user_id: user, id: message.id }
-  #                }.to change(user.messages, :count).by(-1)
-  #     end
-  #     it "redirects to the user show page" do
-  #       sign_in user
-  #       delete :destroy, params: { user_id: user, id: message.id }
-  #       expect(response).to redirect_to "/users/#{user.id}/rooms/#{room.id}"
-  #     end
-  #   end
-  #   context "as an non-authenticated user" do
-  #     it "returns a 302 response" do
-  #       delete :destroy, params: { user_id: user, id: message.id }
-  #       expect(response).to have_http_status "302"
-  #     end
-  #   end
-  # end
+  describe "DELETE #destroy" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:room) { Room.create(name: "Chiyoda", room_type: "private") }
+    let!(:participant) { Participant.create(user: user, room: room) }
+    context "as an authenticated user" do
+      it "responds succesfully" do
+        sign_in user
+        expect { delete :destroy, params: { id: user.id, room_id: room.id }
+                 }.to change(room.participants, :count).by(-1)
+      end
+      it "redirects to the user show page" do
+        sign_in user
+        delete :destroy, params: { id: user.id, room_id: room.id }
+        expect(response).to redirect_to "/users/#{user.id}"
+      end
+    end
+    context "as an non-authenticated user" do
+      it "returns a 302 response" do
+        delete :destroy, params: { id: user.id, room_id: room.id }
+        expect(response).to have_http_status "302"
+      end
+    end
+  end
 end
