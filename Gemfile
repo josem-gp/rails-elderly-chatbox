71825344bc436b2efcc6bcac4981b0532100b234
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-24 11:19:13 +0900

    added controller tests for homepage rendering and 200 response

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
+gem 'rails-controller-testing'
 gem 'pg_search', '~> 2.3.0'
 gem 'turbolinks_render'
 gem 'devise'
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
+  describe "#home" do
+    it "responds succesfully" do
+      get :home
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'home' succesfully" do
+      get :home
+      expect(response).to render_template('home')
+    end
+  end
 end
