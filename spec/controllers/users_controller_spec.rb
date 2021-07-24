3eadbf75c6324bfcefa19be490cbbec02665e6c4
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-24 11:45:39 +0900

    added a sign in to the spec with Factory Bot and tested show with Id

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   describe "GET #show" do
     let!(:user) { FactoryBot.create(:user) }
-    let!(:call) { get :show, params: { id: user.id } }
     it "responds succesfully" do
       # expect(response).to have_http_status(:ok) #this is the same as the one under
       sign_in user
+      get :show, params: { id: user.id }
       expect(response).to have_http_status "200"
     end
     it "renders 'home' succesfully" do
       sign_in user
+      get :show, params: { id: user.id }
       expect(response).to render_template('show')
     end
   end
