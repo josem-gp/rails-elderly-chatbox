d1ef03f99fef78060f6878b3c68298e36c22d21c
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-24 17:40:26 +0900

    added a redirection test when creating participant

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -10,6 +10,11 @@ RSpec.describe ParticipantsController, type: :controller do
         expect { post :create, params: { user_id: user.id,
                                          participant: {room_id: room.id} } }.to change(room.participants, :count).by(1)
       end
+      it "redirects to room show page" do
+        sign_in user
+        post :create, params: { user_id: user.id, participant: {room_id: room.id} }
+        expect(response).to redirect_to "/users/#{user.id}/rooms/#{room.id}"
+      end
     end
     context "as a non-authenticated user" do
       it "returns a 302 response" do
@@ -18,4 +23,26 @@ RSpec.describe ParticipantsController, type: :controller do
       end
     end
   end
+  # describe "DELETE #destroy" do
+  #   let(:user) { FactoryBot.create(:user) }
+  #   let(:room) { Room.create(name: "Chiyoda", room_type: "private") }
+  #   context "as an authenticated user" do
+  #     it "responds succesfully" do
+  #       sign_in user
+  #       expect { delete :destroy, params: { user_id: user, id: message.id }
+  #                }.to change(user.messages, :count).by(-1)
+  #     end
+  #     it "redirects to the user show page" do
+  #       sign_in user
+  #       delete :destroy, params: { user_id: user, id: message.id }
+  #       expect(response).to redirect_to "/users/#{user.id}/rooms/#{room.id}"
+  #     end
+  #   end
+  #   context "as an non-authenticated user" do
+  #     it "returns a 302 response" do
+  #       delete :destroy, params: { user_id: user, id: message.id }
+  #       expect(response).to have_http_status "302"
+  #     end
+  #   end
+  # end
 end
