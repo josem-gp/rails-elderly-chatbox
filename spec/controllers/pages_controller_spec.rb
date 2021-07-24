efdba7611ee48556d2772dc14a3c6047c772a65f
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-24 11:20:59 +0900

    refactor controller pages test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-  describe "#home" do
+  describe "GET #home" do
+    let!(:call) { get :home }
     it "responds succesfully" do
-      get :home
       expect(response).to have_http_status(:ok)
     end
     it "renders 'home' succesfully" do
-      get :home
       expect(response).to render_template('home')
     end
   end
