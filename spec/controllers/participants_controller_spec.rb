fe766b498e769960d3c0eb0747790adfca3650c8
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-24 17:36:13 +0900

    added test to create for participant controller

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ParticipantsController, type: :controller do
-
+  describe "POST #show" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:room) { Room.create(name: "Chiyoda", room_type: "private") }
+    context "as an authenticated user" do
+      it "responds succesfully" do
+        sign_in user
+        expect { post :create, params: { user_id: user.id,
+                                         participant: {room_id: room.id} } }.to change(room.participants, :count).by(1)
+      end
+    end
+    context "as a non-authenticated user" do
+      it "returns a 302 response" do
+        # get :show, params: { id: room.id, user_id: user.id}
+        # expect(response).to have_http_status "302"
+      end
+    end
+  end
 end
