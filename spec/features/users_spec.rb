e3f0a9c6ebb4beee4f7571151e9b72661d9b91f6
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-25 13:39:02 +0900

    added support for devise for automatic log in when using capybara

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
-  scenario "user logs out" do
+  scenario "user clicks in a room tag" do
     user = FactoryBot.create(:user)
+    login_as(user)
     visit "users/#{user.id}"
-    within ".dropdown-item" do
-      click_link "Log out"
-    end
-    expect(page).to have_text('Signed out successfully')
+    save_and_open_page #cuando miro esto, veo que no se han asignado las "rooms". quizás tenga que hacerlo manualmente ya que es un test
+    find(:xpath, "/html/body/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div/div[1]/div/a")
   end
 end
