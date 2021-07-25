880ce2373a5f275f6b933266f2280fb5dc4ad5bb
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-25 19:46:46 +0900

    added feature scenario for joining a room

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -16,5 +16,24 @@ RSpec.feature "Users", type: :feature do
     sleep 1 #so that the modal appears
     expect(page).to have_text "Join a Room"
   end
-
+  scenario "user joins a room", js:true do
+    user = FactoryBot.create(:user, :with_rooms)
+    Room.create(name: "Fussa", room_type: "private")
+    login_as(user)
+    visit "users/#{user.id}"
+    find(:xpath, "/html/body/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[3]/div/div[1]").click
+    sleep 1 #so that the modal appears
+    expect{
+      select "Fussa", from: "Room"
+      click_button "Join"
+    }.to change(user.rooms, :count).by(1)
+  end
+  scenario "user clicks in the vertical navbar", js:true do
+    user = FactoryBot.create(:user, :with_rooms)
+    login_as(user)
+    visit "users/#{user.id}"
+    find(:xpath, "/html/body/div/div[1]/ul/li[2]/a").click
+    sleep 1
+    expect(page).to have_text "My Page"
+  end
 end
