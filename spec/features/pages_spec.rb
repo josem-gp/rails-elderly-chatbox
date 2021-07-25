d78f132288e38a1b4fe46d6cd5e963e4181ece05
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-25 11:56:24 +0900

    finished features spec for homepage log in and sign up

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Pages", type: :feature do
   scenario "user logs in" do
-    user = FactoryBot.create(:user)
+    user = FactoryBot.create(:user, email: 'tester@tester.com', alias: 'noalias')
 
     visit root_path
     click_link  "Log in"
@@ -13,6 +13,18 @@ RSpec.feature "Pages", type: :feature do
     expect(page).to have_current_path "/users/#{user.id}"
   end
   scenario "guest signs up" do
+    visit root_path
+    click_link "Sign up"
+    fill_in "Name", with: "jose"
+    fill_in "Alias", with: "jgptest"
+    select "Adachi-ku", from: "Municipality"
+    fill_in "Email", with: "test@test.com"
+    within ".user_password" do
+      fill_in 'Password', with: 'password' #it didnt work with a simply fill_in so i had to use a within
+    end
+    click_button "Create a new account"
+    # save_and_open_page #=> for debugging purposes
 
+    expect(page).to have_text('Welcome! You have signed up successfully.')
   end
 end
