0bdc0515d8f5696461426709ffc5a0c89206e8b8
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-25 18:42:27 +0900

    used headless chrome to test js actions when clicking modals

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -5,6 +5,16 @@ RSpec.feature "Users", type: :feature do
     user = FactoryBot.create(:user, :with_rooms)
     login_as(user)
     visit "users/#{user.id}"
-    find(:xpath, "/html/body/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div/div[1]/div/a")
+    find(:xpath, "/html/body/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div/div[1]/div/a").click
+    expect(page).to have_current_path "/users/#{user.id}/rooms/#{user.rooms[0].id}"
   end
+  scenario "user clicks in a modal button", js:true do
+    user = FactoryBot.create(:user, :with_rooms)
+    login_as(user)
+    visit "users/#{user.id}"
+    find(:xpath, "/html/body/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[3]/div/div[1]").click
+    sleep 1 #so that the modal appears
+    expect(page).to have_text "Join a Room"
+  end
+
 end
