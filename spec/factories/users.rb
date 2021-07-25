a348ebbfe7d91269455a4d7b3dbcf9e6b5cffe11
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-25 12:49:03 +0900

    changed factory user so that there are no duplicates throughout the specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :user do
     name {"Tester"}
     # self.alias {"tester"}
-    sequence(:alias) { |n| "tester#{n}" }
+    sequence(:alias) { |n| "tester#{n+1}" }
     municipality {"Chiyoda-ku"}
-    sequence(:email) { |n| "test#{n}@example.com" }
+    sequence(:email) { |n| "test#{n+1}@example.com" }
     password {"1234567"}
     # icon {create(:icon)}
     ##since icon is connected with user we can use associations (three ways):
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
-  user = FactoryBot.create(:user, email: 'tester3@tester.com', alias: 'noalias3')
   scenario "user logs out" do
+    user = FactoryBot.create(:user)
     visit "users/#{user.id}"
-    click_link "Log out"
+    within ".dropdown-item" do
+      click_link "Log out"
+    end
     expect(page).to have_text('Signed out successfully')
   end
 end
