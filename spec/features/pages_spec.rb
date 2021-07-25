dcd770e5d356d306f837804b4b43cb6425f113f5
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-25 11:20:54 +0900

    added feature for log in from homepage

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "Pages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "user logs in" do
+    user = FactoryBot.create(:user)
+
+    visit root_path
+    click_link  "Log in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect(page).to have_current_path "/users/#{user.id}"
+  end
+  scenario "guest signs up" do
+
+  end
 end
